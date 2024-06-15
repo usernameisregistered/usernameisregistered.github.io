662d192926c53f241bb5d4b239044a1103d975f8
usernameisregistered <liming_longxi@163.com>
2024-06-15 13:46:32 +0800

    [modify]更新内容

--- submit.bat
+++ submit.bat
@@ -1,21 +1,22 @@
 chcp 65001
 @echo off
 setlocal
- 
+title "开始提交github"
 :: 配置用户信息
-git config user.name "liming"
+git config user.name "usernameisregistered"
 git config user.email "liming_longxi@163.com"
  
 :: 添加所有变更到暂存区
 git add .
- 
-:: 提交变更，其中"%*"是批处理参数，可以在调用批处理时传入提交信息
 git commit -m "[modify]更新内容"
-
-:: 推送到远程仓库
-git push
 git push gitlab
-git push origin
+title "开始提交gitlab"
+
+git config user.name "liming"
+git config user.email "liming_longxi@163.com"
+:: 推送到远程仓库
+git push gitlab
+git push github
  
 :: 清理环境变量git
 endlocal
